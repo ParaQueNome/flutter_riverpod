1449a5bc2bd881a7418246f7d29fbb742436da28
ParaQueNome <guiagusto455@gmail.com>
2024-10-21 14:45:49 -0300

    Adicionando providers e implementacao repository

--- lib/features/products/data/product_repository_impl.dart
+++ lib/features/products/data/product_repository_impl.dart
@@ -1,1 +1,24 @@
 //TODO: Criar implementação do repositório de produtos
+import 'package:dio/dio.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:from_zero_to_hero_ht/features/products/presentation/providers/dio_provider.dart';
+import 'package:from_zero_to_hero_ht/features/products/domain/product_repository.dart';
+
+part 'product_repository_impl.g.dart';
+
+class ProductRepositoryImpl implements ProductRepository{
+  final Dio dio;
+  
+  ProductRepositoryImpl({required this.dio});
+
+  Future<List<String>> findAll() async{
+    final response = await dio.get('/categories');
+    return[for(final category in response.data as List) category.toString()];
+
+  }
+}
+
+@riverpod
+ProductRepositoryImpl productRepository(ProductRepositoryRef ref){
+  return ProductRepositoryImpl(dio: ref.watch(dioProvider));
+}
--- lib/features/products/presentation/providers/category_provider.dart
+++ lib/features/products/presentation/providers/category_provider.dart
@@ -1,1 +1,12 @@
 //TODO: Criar o provedor de categorias
+
+import 'package:from_zero_to_hero_ht/features/products/data/product_repository_impl.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+
+
+@riverpod
+FutureOr<List<String>> categories(CategoriesRef ref) async {
+  final categories = await ref.watch(productRepositoryProvider).findAllCategories();
+  return categories;
+}
